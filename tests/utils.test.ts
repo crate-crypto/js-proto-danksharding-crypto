bd7392aca04bc4647c2429448c3b94136f9b0eed
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-02-28 09:55:10 +0000

    add assertScalar

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,9 +1,13 @@
 import { expect } from "chai";
 import { Bytes48, Bytes96 } from "../src/primitives/bytearrays.js";
 import { G1Point, G2Point } from "../src/primitives/points.js";
+import { Scalar } from "../src/primitives/field.js";
 
 export function assertArrEqual(lhs: Uint8Array, rhs: Uint8Array) {
     expect(lhs.toString()).to.equal(rhs.toString())
+}
+export function assertScalarEqual(lhs: Scalar, rhs: Scalar) {
+    expect(lhs.toBytes().toString()).to.equal(rhs.toBytes().toString())
 }
 export function assertBytes48Equal(lhs: Bytes48, rhs: Bytes48) {
     assertArrEqual(lhs.toBytes(), rhs.toBytes())
