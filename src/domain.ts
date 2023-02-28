502c1078e79a366301cd812993679d63b7687862
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-02-28 10:11:47 +0000

    add domain

---
+++ src/domain.ts
@@ -0,0 +1,91 @@
+import { hexToBytes } from "@noble/hashes/utils";
+import { FIELD_ELEMENTS_PER_BLOB } from "./constants.js";
+import { Scalar } from "./primitives/field.js";
+import { Polynomial } from "./primitives/polynomial.js";
+import reversedRootsOfUnity from './reversedRootsOfUnity.json' assert {type: "json"};
+import { assertNoErrorThrow } from "./utils.js";
+
+// Domain stores a list of all of the points that
+// we want to evaluate our polynomials at. 
+// When we specify a polynomial is in lagrange/evaluation form
+// this is the domain to which that polynomial is over.
+export class Domain {
+    rootsOfUnity: Scalar[]
+    _size: Scalar
+    sizeInv: Scalar
+
+    private constructor(roots: Scalar[]) {
+        this.rootsOfUnity = roots
+        // Safe: we can safely assume that the number of roots 
+        // will not exceed the size of the scalar Scalar.
+        // If this happens, then the integer is reduced
+        this._size = Scalar.fromBigIntReduce(BigInt(roots.length))
+        this.sizeInv = assertNoErrorThrow(Scalar.invert(this._size))
+
+    }
+    // Returns a domain from a JSON file which has already reversed
+    // them according to the reverse methods in the specs.
+    //
+    // Note: We can get them unreversed and implement those
+    // reversing methods.
+    public static fromJson(): Domain {
+        let numRoots = reversedRootsOfUnity.length
+        if (numRoots != FIELD_ELEMENTS_PER_BLOB) {
+            throw new Error("mismatch between the number of roots in the domain and the degree of polynomial")
+        }
+        let roots: Scalar[] = new Array()
+        for (let i = 0; i < FIELD_ELEMENTS_PER_BLOB; i++) {
+            let byteArr = hexToBytes(reversedRootsOfUnity[i])
+            // This throw is okay as it happens at compile time
+            // and will only happen if the json file is corrupt
+            let fieldErr = Scalar.fromBytesChecked(byteArr)
+            if (fieldErr instanceof Error) throw fieldErr;
+
+            roots.push(fieldErr as Scalar)
+        }
+        return new Domain(roots)
+    }
+    // Returns the size of the domain
+    // Due to the if guard in fromJson, this will always
+    // be `FIELD_ELEMENTS_PER_BLOB`
+    public size(): bigint {
+        return this._size.integer
+    }
+    // Evaluates a polynomial in lagrange form
+    // The evaluation point can either be in the domain or not
+    public evaluatePolyInEvalForm(polynomial: Polynomial, inputPoint: Scalar): Scalar {
+        // Check if element is in the domain
+        let index = this.findIndexOfElement(inputPoint)
+        if (index != -1) {
+            // The point is in the domain, so we can evaluate 
+            // by indexing the polynomial
+            return polynomial.index(index)
+        }
+
+        let result = Scalar.zero()
+        for (let i = 0; i < FIELD_ELEMENTS_PER_BLOB; i++) {
+            let a = Scalar.mul(polynomial.index(i), this.rootsOfUnity[i])
+            let b = Scalar.sub(inputPoint, this.rootsOfUnity[i])
+            // We throw here because `b` can only be `0`
+            // if the `inputPoint` is in the domain, which we have checked
+            let aDivB = assertNoErrorThrow(Scalar.div(a, b))
+            result = Scalar.add(result, aDivB)
+        }
+        result = Scalar.mul(result, this.sizeInv)
+
+        // Compute inputPoint^size -1
+        let inputPowWidthMinusOne = Scalar.pow(inputPoint, this._size)
+        inputPowWidthMinusOne = Scalar.sub(inputPowWidthMinusOne, Scalar.one())
+
+        return Scalar.mul(result, inputPowWidthMinusOne)
+    }
+
+    // TODO: benchmark this to see how fast it is
+    // TODO see https://stackoverflow.com/questions/23500591/javascript-fast-lookup-data-structure
+    public findIndexOfElement(element: Scalar): number {
+        // Equality predicate -- cannot use IndexOf as that will check for 
+        // references.
+        const equality = (arrElement: Scalar) => arrElement.integer === element.integer;
+        return this.rootsOfUnity.findIndex(equality)
+    }
+}
---
+++ tests/domain.test.ts
@@ -0,0 +1,72 @@
+import { Blob } from '../src/primitives/blob.js'
+import { Scalar } from '../src/primitives/field.js'
+import { Domain } from '../src/domain.js'
+import { expect } from 'chai'
+import { FIELD_ELEMENTS_PER_BLOB } from '../src/constants.js';
+import { Polynomial } from '../src/primitives/polynomial.js'
+import { assertArrEqual, assertScalarEqual } from './utils.test.js';
+
+describe('domain', () => {
+    it('domain can be constructed', () => {
+        let domain = Domain.fromJson()
+        expect(domain.size()).to.equal(BigInt(FIELD_ELEMENTS_PER_BLOB))
+    })
+    it('find element in domain', () => {
+        let domain = Domain.fromJson()
+        // Clone the domain, because there are equality methods
+        // in js, which will check equality `===` by checking if
+        // the object is exactly the same object, meaning the same 
+        // memory reference. Whereas we want value equality.
+        let domain_clone = Domain.fromJson()
+        let roots: Scalar[] = domain_clone.rootsOfUnity
+
+        for (let i = 0; i < domain.size(); i++) {
+            let rootInDomain = roots[i]
+            let index = domain.findIndexOfElement(rootInDomain)
+            expect(index).to.not.equal(-1, "element was in domain, but find method returned not found")
+            expect(index).to.equal(i)
+        }
+        // zero is not a root of unity, so we expect to get -1
+        let zero = Scalar.fromBigIntReduce(0n)
+        expect(domain.findIndexOfElement(zero)).to.equal(-1)
+
+        // -1 is also not a root of unity for this usecase
+        // so we also expect a -1
+        let minusOne = Scalar.fromBigIntReduce(-1n)
+        expect(domain.findIndexOfElement(minusOne)).to.equal(-1)
+
+        // `1` is a root of unity and we expect it to be the first one
+        // Even after reversing the domain
+        let one = Scalar.one()
+        expect(domain.findIndexOfElement(one)).to.equal(0)
+    })
+    it('evaluate polynomial in/out domain', () => {
+        let domain = Domain.fromJson()
+
+        let polynomial = dummyPolynomial(domain.size())
+
+        // Evaluate the polynomial at all points on the domain
+        for (let i = 0; i < domain.size(); i++) {
+            let root = domain.rootsOfUnity[i]
+            let gotPolyOutput = domain.evaluatePolyInEvalForm(polynomial, root)
+            let expectedPolyOutput = polynomial.index(i)
+            assertScalarEqual(gotPolyOutput, expectedPolyOutput)
+        }
+
+        // Evaluate on a point not in the domain
+        let evaluation = Scalar.fromBigIntReduce(1234n)
+        let expectedPolyOutput = new Uint8Array([63, 110, 105, 211, 238, 35, 205, 240, 222, 86, 44, 39, 223, 10, 75, 110, 123, 36, 69, 239, 44, 146, 89, 38, 170, 68, 122, 16, 201, 191, 22, 49])
+        let gotPolyOutput = domain.evaluatePolyInEvalForm(polynomial, evaluation)
+        assertArrEqual(gotPolyOutput.toBytes(), expectedPolyOutput)
+    })
+})
+
+function dummyPolynomial(numEvaluations: bigint): Polynomial {
+    // Create a polynomial whose evaluations are 0 to numEvaluations-1
+    let evaluations = new Array()
+    for (let i = 0; i < numEvaluations; i++) {
+        let evaluation = Scalar.fromBigIntReduce(BigInt(i))
+        evaluations.push(evaluation)
+    }
+    return new Polynomial(evaluations)
+}
