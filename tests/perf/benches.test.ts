d2406c51381372150f819c878e71b2f7d2f983ff
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-01 18:04:19 +0000

    add benchmark setup

---
+++ tests/perf/benches.test.ts
@@ -0,0 +1,14 @@
+import { itBench } from "@dapplion/benchmark"
+import { Context } from "../../src/index.js";
+import { dummyPolynomial } from "../utils.test.js";
+
+describe("Prover", () => {
+    let context = new Context()
+
+    let poly = dummyPolynomial(1234n)
+    let blob = poly.toBlob().toBytes()
+
+    itBench("compute blob proof", () => {
+        let _ = context.computeBlobKZGProf(blob)
+    })
+})
