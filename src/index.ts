0e5681fcb04aaa460d9bc3762de84127a9c332a6
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-01 10:42:27 +0000

    use try-catch to simplify error handling

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { OpenKey, VerifyKZGBatch, VerifyKZGBatchNaive } from "./openKey.js"
 import { Bytes32, Bytes48, Blob, Polynomial, G1Point, Scalar, G1AffinePoint } from "./primitives/index.js"
 import { computeKZGProofImpl } from "./kzg.js"
 import { computeChallenge } from "./fiatshamir.js"
+import { assertNoErrorThrow } from "./utils.js"
 
 // A serialized commitment to a polynomial.
 export type Commitment = Bytes48
@@ -28,133 +29,121 @@ export class Context {
         this.openingKey = OpenKey.fromJson()
         this.commitKey = CommitKey.fromJson()
     }
+
     // See: https://github.com/ethereum/consensus-specs/blob/ad58bfc3044fa1d8a8331e2741f8e78a6db795e2/specs/deneb/polynomial-commitments.md#bytes_to_kzg_commitment
     public blobToKZGCommitment(blob: Blob): Commitment | Error {
-        // 1. Deserialize and perform validation checks
-        //
-        let polynomialErr = Polynomial.fromBlob(blob)
-        if (polynomialErr instanceof Error) return polynomialErr;
-        let polynomial = polynomialErr as Polynomial
+        try {
+            // 1. Deserialize and perform validation checks
+            //
+            let polynomial = assertNoErrorThrow(Polynomial.fromBlob(blob))
 
-        // 2. Commit to polynomial
-        //
-        let commitmentPoint = this.commitKey.commitUnsafe(polynomial)
+            // 2. Commit to polynomial
+            //
+            let commitmentPoint = this.commitKey.commitUnsafe(polynomial)
 
-        // 3. Serialize commitment
-        //
-        return commitmentPoint.toBytes48()
+            // 3. Serialize commitment
+            //
+            return commitmentPoint.toBytes48()
+        } catch (error) {
+            return error as Error
+        }
     }
 
     // See: https://github.com/ethereum/consensus-specs/blob/ad58bfc3044fa1d8a8331e2741f8e78a6db795e2/specs/deneb/polynomial-commitments.md#compute_kzg_proof
     public computeKZGProof(blob: Blob, z: Bytes32): { claimedValue: Scalar, proof: KZGProof } | Error {
-        // 1. Deserialize and perform validation checks
-        //
-        let polynomialErr = Polynomial.fromBlob(blob)
-        let zScalarErr = Scalar.fromBytes32Checked(z)
+        try {
+            // 1. Deserialize and perform validation checks
+            //
+            let polynomial = assertNoErrorThrow(Polynomial.fromBlob(blob))
+            let zScalar = assertNoErrorThrow(Scalar.fromBytes32Checked(z))
 
-        if (polynomialErr instanceof Error) return polynomialErr;
-        if (zScalarErr instanceof Error) return zScalarErr;
+            // 2. Compute KZG proof
+            //
+            let proof = assertNoErrorThrow(computeKZGProofImpl(this.domain, this.commitKey, polynomial, zScalar))
 
-        let polynomial = polynomialErr as Polynomial
-        let zScalar = zScalarErr as Scalar
-
-        // 2. Compute KZG proof
-        //
-        let proofErr = computeKZGProofImpl(this.domain, this.commitKey, polynomial, zScalar)
-        if (proofErr instanceof Error) return proofErr;
-        let proof = proofErr as { claimedValue: Scalar, quotient: G1Point }
-
-        // 3. Serialize proof
-        //
-        return { claimedValue: proof.claimedValue, proof: proof.quotient.toBytes48() }
+            // 3. Serialize proof
+            //
+            return { claimedValue: proof.claimedValue, proof: proof.quotient.toBytes48() }
+        } catch (error) {
+            return error as Error
+        }
     }
 
     // See: https://github.com/ethereum/consensus-specs/blob/ad58bfc3044fa1d8a8331e2741f8e78a6db795e2/specs/deneb/polynomial-commitments.md#compute_blob_kzg_proof
     public computeBlobKZGProf(blob: Blob): { claimedValue: Bytes32, commitment: Commitment, proof: KZGProof } | Error {
-        // 1. Deserialize and perform validation checks
-        //
-        let polynomialErr = Polynomial.fromBlob(blob)
-        if (polynomialErr instanceof Error) return polynomialErr;
-        let polynomial = polynomialErr as Polynomial
+        try {
+            // 1. Deserialize and perform validation checks
+            //
+            let polynomial = assertNoErrorThrow(Polynomial.fromBlob(blob))
 
-        // 2. Commit to polynomial
-        //
-        let commitmentPoint = this.commitKey.commitUnsafe(polynomial)
-        let commitment = commitmentPoint.toBytes48()
+            // 2. Commit to polynomial
+            //
+            let commitmentPoint = this.commitKey.commitUnsafe(polynomial)
+            let commitment = commitmentPoint.toBytes48()
 
-        // 3. Compute evaluation challenge
-        //
-        let evalChallenge = computeChallenge(blob, commitment)
+            // 3. Compute evaluation challenge
+            //
+            let evalChallenge = computeChallenge(blob, commitment)
 
-        // 4. Compute KZG proof
-        //
-        let proofErr = computeKZGProofImpl(this.domain, this.commitKey, polynomial, evalChallenge)
-        if (proofErr instanceof Error) return proofErr;
-        let proof = proofErr as { claimedValue: Scalar, quotient: G1Point }
+            // 4. Compute KZG proof
+            //
+            let proof = assertNoErrorThrow(computeKZGProofImpl(this.domain, this.commitKey, polynomial, evalChallenge))
 
-        // 5. Serialize proof data
-        //
-        return { claimedValue: proof.claimedValue.toBytes32(), commitment: commitment, proof: proof.quotient.toBytes48() }
+            // 5. Serialize proof data
+            //
+            return { claimedValue: proof.claimedValue.toBytes32(), commitment: commitment, proof: proof.quotient.toBytes48() }
+        } catch (error) {
+            return error as Error
+        }
+
     }
 
     // See: https://github.com/ethereum/consensus-specs/blob/ad58bfc3044fa1d8a8331e2741f8e78a6db795e2/specs/deneb/polynomial-commitments.md#verify_kzg_proof
     public verifyKZGProof(commitment: Bytes48, z: Bytes32, y: Bytes32, proof: Bytes48): void | Error {
+        try {
+            // 1. Deserialize and perform validation checks
+            //
+            let commitmentPoint = assertNoErrorThrow(G1Point.fromBytes48Checked(commitment))
+            let proofPoint = assertNoErrorThrow(G1Point.fromBytes48Checked(proof))
+            let zScalar = assertNoErrorThrow(Scalar.fromBytes32Checked(z))
+            let yScalar = assertNoErrorThrow(Scalar.fromBytes32Checked(y))
 
-        // 1. Deserialize and perform validation checks
-        //
-        let commitmentPointErr = G1Point.fromBytes48Checked(commitment)
-        let proofPointErr = G1Point.fromBytes48Checked(proof)
-        let zScalarErr = Scalar.fromBytes32Checked(z)
-        let yScalarErr = Scalar.fromBytes32Checked(y)
-
-        if (commitmentPointErr instanceof Error) return commitmentPointErr;
-        if (proofPointErr instanceof Error) return proofPointErr;
-        if (zScalarErr instanceof Error) return zScalarErr;
-        if (yScalarErr instanceof Error) return yScalarErr;
-
-        let commitmentPoint = commitmentPointErr as G1Point
-        let proofPoint = proofPointErr as G1Point
-        let zScalar = zScalarErr as Scalar
-        let yScalar = yScalarErr as Scalar
-
-        // 2. Verify KZG proof
-        //
-        let valid = this.openingKey.verify(commitmentPoint, zScalar, yScalar, proofPoint)
-        if (valid == false) {
-            return new Error("proof verification returned false")
+            // 2. Verify KZG proof
+            //
+            let valid = this.openingKey.verify(commitmentPoint, zScalar, yScalar, proofPoint)
+            if (valid == false) {
+                return new Error("proof verification returned false")
+            }
+        } catch (error) {
+            return error as Error
         }
     }
 
     // See: https://github.com/ethereum/consensus-specs/blob/ad58bfc3044fa1d8a8331e2741f8e78a6db795e2/specs/deneb/polynomial-commitments.md#verify_blob_kzg_proof
     public verifyBlobKZGProof(blob: Blob, commitment: Bytes48, proof: Bytes48): void | Error {
-        // 1. Deserialize and perform validation checks
-        //
-        let polynomialErr = Polynomial.fromBlob(blob)
-        let commitmentPointErr = G1Point.fromBytes48Checked(commitment)
-        let proofPointErr = G1Point.fromBytes48Checked(proof)
+        try {
+            // 1. Deserialize and perform validation checks
+            //
+            let polynomial = assertNoErrorThrow(Polynomial.fromBlob(blob))
+            let commitmentPoint = assertNoErrorThrow(G1Point.fromBytes48Checked(commitment))
+            let proofPoint = assertNoErrorThrow(G1Point.fromBytes48Checked(proof))
 
-        if (polynomialErr instanceof Error) return polynomialErr;
-        if (commitmentPointErr instanceof Error) return commitmentPointErr;
-        if (proofPointErr instanceof Error) return proofPointErr;
+            // 2. Compute evaluation challenge
+            //
+            let evalChallenge = computeChallenge(blob, commitment)
 
-        let polynomial = polynomialErr as Polynomial
-        let commitmentPoint = commitmentPointErr as G1Point
-        let proofPoint = proofPointErr as G1Point
+            // 3. Evaluate polynomial
+            //
+            let polyEvaluation = this.domain.evaluatePolyInEvalForm(polynomial, evalChallenge)
 
-
-        // 2. Compute evaluation challenge
-        //
-        let evalChallenge = computeChallenge(blob, commitment)
-
-        // 3. Evaluate polynomial
-        //
-        let polyEvaluation = this.domain.evaluatePolyInEvalForm(polynomial, evalChallenge)
-
-        // 4. Verify KZG proof
-        //
-        let valid = this.openingKey.verify(commitmentPoint, evalChallenge, polyEvaluation, proofPoint)
-        if (valid == false) {
-            return new Error("proof verification returned false")
+            // 4. Verify KZG proof
+            //
+            let valid = this.openingKey.verify(commitmentPoint, evalChallenge, polyEvaluation, proofPoint)
+            if (valid == false) {
+                return new Error("proof verification returned false")
+            }
+        } catch (error) {
+            return error as Error
         }
     }
 
@@ -166,29 +155,26 @@ export class Context {
         let inputPoints: Scalar[] = []
         let claimedValues: Scalar[] = []
 
-        for (let i = 0; i < blobs.length; i++) {
-            let blob = blobs[i]
-            let commitment = commitments[i]
+        try {
+            for (let i = 0; i < blobs.length; i++) {
+                let blob = blobs[i]
+                let commitment = commitments[i]
+                let proof = proofs[i]
 
-            let polynomialErr = Polynomial.fromBlob(blobs[i])
-            let commitmentErr = G1AffinePoint.fromBytesChecked(commitments[i].toBytes())
-            let proofErr = G1AffinePoint.fromBytesChecked(proofs[i].toBytes())
+                let polynomial = assertNoErrorThrow(Polynomial.fromBlob(blob))
+                let commitmentPoint = assertNoErrorThrow(G1AffinePoint.fromBytesChecked(commitment.toBytes()))
+                let proofPoint = assertNoErrorThrow(G1AffinePoint.fromBytesChecked(proof.toBytes()))
 
-            if (polynomialErr instanceof Error) return polynomialErr;
-            if (commitmentErr instanceof Error) return commitmentErr;
-            if (proofErr instanceof Error) return proofErr;
+                let evaluationChallenge = computeChallenge(blob, commitment)
+                let outputPoint = this.domain.evaluatePolyInEvalForm(polynomial, evaluationChallenge)
 
-            let polynomial = polynomialErr as Polynomial
-            let commitmentPoint = commitmentErr as G1AffinePoint
-            let proofPoint = proofErr as G1AffinePoint
-            let evaluationChallenge = computeChallenge(blob, commitment)
-
-            let outputPoint = this.domain.evaluatePolyInEvalForm(polynomial, evaluationChallenge)
-
-            commitmentPoints.push(commitmentPoint)
-            proofPoints.push(proofPoint)
-            inputPoints.push(evaluationChallenge)
-            claimedValues.push(outputPoint)
+                commitmentPoints.push(commitmentPoint)
+                proofPoints.push(proofPoint)
+                inputPoints.push(evaluationChallenge)
+                claimedValues.push(outputPoint)
+            }
+        } catch (error) {
+            return error as Error
         }
 
         return VerifyKZGBatch(this.openingKey, commitmentPoints, inputPoints, claimedValues, proofPoints)
