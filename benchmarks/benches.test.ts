56bbe1b7d47f95baff261d52e8405b5c179ff540
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-03 22:07:26 +0000

    fix: benchmark

--- benchmarks/benches.test.ts
+++ benchmarks/benches.test.ts
@@ -2,7 +2,7 @@ import { itBench } from "@dapplion/benchmark"
 import { Context } from "../src/index.js";
 import { dummyPolynomial } from "../tests/utils.test.js";
 
-import { computeBlobKzgProof, loadTrustedSetup, transformTrustedSetupJSON, computeKzgProof, blobToKzgCommitment, verifyBlobKzgProof, verifyBlobKzgProofBatch } from "c-kzg"
+import { computeBlobKzgProof, loadTrustedSetup, transformTrustedSetupJSON, computeKzgProof, blobToKzgCommitment, verifyBlobKzgProof, verifyBlobKzgProofBatch, verifyKzgProof } from "c-kzg"
 import { Scalar } from "../src/primitives/field.js";
 const SETUP_FILE_PATH = "./benchmarks/testing_trusted_setup.json"
 const file = await transformTrustedSetupJSON(SETUP_FILE_PATH);
@@ -61,7 +61,7 @@ describe("Verifier", () => {
         let _ = context.verifyKZGProof(commitment, zScalarBytes, zScalarBytes, proof)
     })
     itBench("verify kzg proof -- c-kzg", () => {
-        let _ = context.verifyKZGProof(commitment, zScalarBytes, zScalarBytes, proof)
+        let _ = verifyKzgProof(commitment, zScalarBytes, zScalarBytes, proof)
     })
 
     itBench("verify batch kzg proof (2) -- this lib", () => {
